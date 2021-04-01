93a69676ace1c6b34e784d974ca04593d9813107
sjkim <ksj10111011@gmail.com>
2021-04-01 13:45:01 +0900

    partial_form 연습

---
+++ csharp_study_1/csharp_study_1/csharp_study_1/xaml_config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace csharp_study_1
+{
+    partial class xaml_config
+    {
+        public static string _TEXT_DATA = "TEXT_DATA";
+        public static string _CBOX_DATA = "CBOX_DATA";
+        public static string _NUMB_DATA = "NUMB_DATA";
+
+    }
+
+
+}
---
+++ csharp_study_1/csharp_study_1/csharp_study_1/xaml_config_1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace csharp_study_1
+{
+    partial class xaml_config
+    {
+        public Dictionary<string, string> XmlRead(string path)
+        {
+            Dictionary<string, string> ret = new Dictionary<string, string>();
+
+            using (XmlReader reader = XmlReader.Create(path))
+            {
+                while (reader.Read() == true)
+                {
+                    if (reader.IsStartElement() == true)
+                    {
+                        if (reader.Name.Equals("SETTING"))
+                        {
+                            string str_id = reader["ID"];
+                            reader.Read();
+
+                            string text_data_buff = reader.ReadElementContentAsString(_TEXT_DATA, "");
+                            ret.Add(_TEXT_DATA, text_data_buff);
+
+                            string cbox_data_buff = reader.ReadElementContentAsString(_CBOX_DATA, "");
+                            ret.Add(_CBOX_DATA, cbox_data_buff);
+                        }
+                    }
+                }
+            }
+
+            return ret;
+        }
+
+        public void XMLWrit(string path, Dictionary<string, string> xml_config)
+        {
+
+        }
+    }
+}
